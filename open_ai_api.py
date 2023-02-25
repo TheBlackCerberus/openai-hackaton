9266f864a15b6e28e7368e42d98522ae643228f3
Arun Nama <arun.nama@gmail.com>
2023-02-25 16:09:28 +0100

    frontend

--- open_ai_api.py
+++ open_ai_api.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get(r'/openai/{prompt}')
 def code_review(prompt: str = Path(None, description='Pass the item_id of the item you want to view') ):
-    API_KEY = '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'
+    API_KEY = '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'
 
     openai.api_key = API_KEY
     model = 'text-davinci-003'
