f031f1f8b5b64192fd3354cb52e8bc2f776bf581
dutchscraper <110257212+dutchscraper@users.noreply.github.com>
2023-02-25 16:53:49 +0100

    Update open_ai_api.py

--- open_ai_api.py
+++ open_ai_api.py
@@ -1,13 +1,15 @@
 import openai
-
-
+import requests
 from fastapi import FastAPI, Path
+from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
 @app.get(r'/openai/{prompt}')
 def code_review(prompt: str = Path(None, description='Pass the item_id of the item you want to view') ):
-    API_KEY = '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'
+    API_KEY = ''
 
     openai.api_key = API_KEY
     model = 'text-davinci-003'
@@ -20,6 +22,29 @@ def code_review(prompt: str = Path(None, description='Pass the item_id of the it
     return response.choices[0].text
 
 
+
+class Prompt(BaseModel):
+    text: str
+
+
+
+@app.post("/items/")
+async def create_item(prompt: Prompt):
+
+    # return item.name
+    API_KEY = ''
+
+    openai.api_key = API_KEY
+    model = 'text-davinci-003'
+
+    response = openai.Completion.create(
+        prompt=prompt.text,
+        model = 'text-davinci-003'
+    )
+
+    return response.choices[0].text
+
 code_review('How big is the moon?')
-a=2
+# test = requests.post('http://127.0.0.1:8000/items/', json={"text":"how are you today?"})
+
 
