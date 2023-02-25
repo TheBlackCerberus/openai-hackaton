582976720e34499a6bbf3338f6146f57f9d68860
Jeroen Verboom <Jeroen.Verboom@postnl.nl>
2023-02-25 15:13:25 +0100

    Correction

--- open_ai_api.py
+++ open_ai_api.py
@@ -5,13 +5,12 @@ from fastapi import FastAPI, Path
 
 app = FastAPI()
 
-@app.get(r'/')
+@app.get(r'/openai/{prompt}')
 def code_review(prompt: str = Path(None, description='Pass the item_id of the item you want to view') ):
-    API_KEY = ''
+    API_KEY = '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'
 
     openai.api_key = API_KEY
     model = 'text-davinci-003'
-
 
     response = openai.Completion.create(
         prompt=prompt,
@@ -19,6 +18,7 @@ def code_review(prompt: str = Path(None, description='Pass the item_id of the it
     )
 
     return response.choices[0].text
+
 
 code_review('How big is the moon?')
 a=2
