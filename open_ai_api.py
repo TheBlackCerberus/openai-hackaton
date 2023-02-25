6c754d46c6bae5e19c760ba8f7ccfee5956e96d2
Jeroen Verboom <Jeroen.Verboom@postnl.nl>
2023-02-25 14:59:03 +0100

    wrapped it in a fast-api framework

--- open_ai_api.py
+++ open_ai_api.py
@@ -1,17 +1,25 @@
 import openai
 
-API_KEY = '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'
 
-openai.api_key = API_KEY
-model = 'text-davinci-003'
+from fastapi import FastAPI, Path
 
-prompt = 'How big is the moon?'
+app = FastAPI()
 
-response = openai.Completion.create(
-    prompt=prompt,
-    model=model
-)
+@app.get(r'/')
+def code_review(prompt: str = Path(None, description='Pass the item_id of the item you want to view') ):
+    API_KEY = ''
 
+    openai.api_key = API_KEY
+    model = 'text-davinci-003'
+
+
+    response = openai.Completion.create(
+        prompt=prompt,
+        model=model
+    )
+
+    return response.choices[0].text
+
+code_review('How big is the moon?')
 a=2
-
 
